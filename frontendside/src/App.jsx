1a588b3e25c4d89ab4cf50d16433da35056cb2f7
gunjan pandey <pjgunjan111@gmail.com>
2025-03-14 17:10:28 +0530

    deploy

--- frontendside/src/App.jsx
+++ frontendside/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import {Route,BrowserRouter as Router,Routes} from 'react-router-dom';
 import Landing from "./pages/Landing";
 import Authentication from "./pages/Authentication";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import { AuthProvider } from "./contexts/AuthContext";
 import VideoMeet from "./pages/VideoMeet";
 import History from "./pages/History";
