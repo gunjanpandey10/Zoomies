296f2e677d731ef02d2486b65efa37485306225c
gunjan pandey <pjgunjan111@gmail.com>
2025-04-04 13:25:26 +0530

    nav

--- frontendside/src/pages/Landing.jsx
+++ frontendside/src/pages/Landing.jsx
@@ -1,5 +1,6 @@
 import "../App.css";
 import { Link, useNavigate } from "react-router-dom";
+import { NavBar } from "./NavBar";
 
 
 const Landing = () => {
@@ -20,55 +21,27 @@ const Landing = () => {
     //     </nav>  
 
       
-
+<>
 
     
-<div>
 
+<NavBar/>
  
-<nav class="navbar navbar-expand-lg border-bottom" style={{backgroundColor:"#FFF"}}>
-        <div class="container p-2">
-        <a class="navbar-brand" href="#">Zoomie</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-           
-            <form class="d-flex" role="search">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              
 
-              <li class="nav-item">
-                <Link class="nav-link active" to="/ab45">Join as Guest</Link>
-              </li>
-
-              <li class="nav-item">
-                <Link class="nav-link active" to="/auth">Login</Link>
-              </li>
-
-              <li class="nav-item">
-                <Link class="nav-link active" to="/auth">Register</Link>
-              </li>
-             
-            </ul>
-            </form>
-          </div>
-        </div>
-      </nav>
 
 
    <div className="landingMainContainer">
   <div><h1><span style={{color:"#FF9839"}}>Connect</span> with your Loved Ones</h1>
    <p>Cover a Distance by Zoomiee</p>
    <div role="button">
-    <Link to ={"/auth"}  Get Started></Link>
+    <Link to ={"/auth"} Get Started></Link>
    </div>
   </div>  
    <div>
    <img src="/mobile.png" alt=""/>
    </div> 
   </div>
-  </div>
+</>
 
 
   )
