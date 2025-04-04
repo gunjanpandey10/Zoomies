19488f68d12ce44327436ef7386a0b5a07fe67d1
gunjan pandey <pjgunjan111@gmail.com>
2025-04-04 13:12:51 +0530

    landing

--- frontendside/src/pages/Landing.jsx
+++ frontendside/src/pages/Landing.jsx
@@ -25,28 +25,36 @@ const Landing = () => {
     
 <div>
 
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Zoomie</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-       
-        <li class="nav-item">
-          <a class="nav-link" href="/ab45">Join as Guest</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/auth">Login</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/auth">Register</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+ 
+<nav class="navbar navbar-expand-lg border-bottom" style={{backgroundColor:"#FFF"}}>
+        <div class="container p-2">
+        <a class="navbar-brand" href="#">Zoomie</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+           
+            <form class="d-flex" role="search">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              
+
+              <li class="nav-item">
+                <Link class="nav-link active" to="/ab45">Join as Guest</Link>
+              </li>
+
+              <li class="nav-item">
+                <Link class="nav-link active" to="/auth">Login</Link>
+              </li>
+
+              <li class="nav-item">
+                <Link class="nav-link active" to="/auth">Register</Link>
+              </li>
+             
+            </ul>
+            </form>
+          </div>
+        </div>
+      </nav>
 
 
    <div className="landingMainContainer">
