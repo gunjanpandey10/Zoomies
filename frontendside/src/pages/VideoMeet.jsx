63f85935f0107eaba191e0675916b23735d4d062
gunjan pandey <pjgunjan111@gmail.com>
2025-04-06 15:41:53 +0530

    chat

--- frontendside/src/pages/VideoMeet.jsx
+++ frontendside/src/pages/VideoMeet.jsx
@@ -473,7 +473,7 @@ export default function VideoMeetComponent() {
                     {showModal ? <div className={styles.chatRoom}>
 
                         <div className={styles.chatContainer}>
-                            <h1 className='fixed-top'>Chat</h1>
+                            <h1 className='sticky-top'>Chat</h1>
 
                             <div className=" chattingDisplay">
 
